385f5d13b5ee166f2725a30148120046f9d0eae4
Alena Kosheleva <koshelev.alyona@gmail.com>
2018-11-19 15:24:20 -0800

    alena congig properties. again

--- src/main/java/com/project/GetOnboardingStatusMethod.java
+++
@@ -1,9 +0,0 @@
-package com.project;
-
-import com.qaprosoft.carina.core.foundation.api.AbstractApiMethodV2;
-
-public class GetOnboardingStatusMethod extends AbstractApiMethodV2 {
-    public GetOnboardingStatusMethod() {
-        super ();
-    }
-}
--- src/main/java/com/project/utils/Constants.java
+++ src/main/java/com/project/utils/Constants.java
@@ -2,12 +2,9 @@ package com.project.utils;
 
 public interface Constants {
 
-    public interface LOGIN {
+    interface LOGIN {
         String LOGIN_REQ_BODY = "login_rq.json";
         String LOGIN_RES_BODY = "login_rs.json";
     }
 
-    public interface Onboarding {
-        String
-    }
 }
